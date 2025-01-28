e01bdf5c993daf029a2071e53d4ba8e257aa1c70
Glenvin <glenrosario84@gmail.com>
2025-01-29 00:25:19 +0530

    removed extra code

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -10,28 +10,6 @@ router.put('/products/apply-discount', applyDiscount);
 
 router.get('/products/sortLowToHigh', sortLowToHigh);
 
-router.post('/addPorduct', (req, res)=> {
-  
-
-// Create a new product
-const newProduct = new Products({
-  id: 32,
-  name: 'Dyning Table',
-  price: 1500,
-  availableQuantity: 20,
-  image: '/dyning-table'
-});
-
-// Save the product
-newProduct.save()
-  .then(() => {
-    console.log('Product saved successfully');
-  })
-  .catch((error) => {
-    console.error('Error saving product:', error);
-  });
-})
-
 router.get('/products/sortHighTolow' , sortHighToLow)
 
 router.get('/prodcuts/sortByAlphabets', sortByAlphabets)
